b6293483ce91f0079d0b13d53d673b26fdbc1ab4
Ilya Smirnov <ismirnov@aligntech.com>
2023-02-20 20:10:55 +0400

    new: get member in club

--- app/club.py
+++ app/club.py
@@ -3,17 +3,30 @@ import aiohttp
 
 from config import JWT_TOKEN
 
-
 logger = logging.getLogger(__name__)
 VAS3K_ENDPOINT = "https://vas3k.club"
+BY_TELEGRAM_ID = "/by_telegram_id"
 USER = "/user"
 
 
-def parse_membership(user_slug):
+def get_member_by_username(username):
     try:
         async with aiohttp.ClientSession() as session:
             async with session.get(
-                f"{VAS3K_ENDPOINT}{USER}/{user_slug}.json",
+                f"{VAS3K_ENDPOINT}{USER}/{username}.json?service_token={JWT_TOKEN}",
+                params={"jwt": JWT_TOKEN}
+            ) as response:
+                return await response.json()
+    except Exception as e:
+        logger.exception(e)
+        return None
+
+
+def get_member_by_telegram_id(telegram_id):
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(
+                f"{VAS3K_ENDPOINT}{USER}{BY_TELEGRAM_ID}/{telegram_id}.json?service_token={JWT_TOKEN}",
                 params={"jwt": JWT_TOKEN}
             ) as response:
                 return await response.json()
